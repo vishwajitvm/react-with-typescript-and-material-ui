5ecb66b3fd345c5f086c9406023f38f40b4ee19a
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-22 18:31:58 +0530

    Typography - material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import MuiTypographyComponent from './components/MuiTypographyComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <MuiTypographyComponent />
     </div>
   );
 }
---
+++ src/components/MuiTypographyComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const MuiTypographyComponent = () => {
+  return (
+    <div>
+      <Typography variant='h1'> heading one </Typography>
+      <Typography variant='h2'> heading ttwo </Typography>
+      <Typography variant='h3'> heading three </Typography>
+      <Typography variant='h4' gutterBottom> heading four </Typography>
+      <Typography variant='h5'> heading five </Typography>
+      <Typography variant='h6'> heading six </Typography>
+
+      <Typography variant='subtitle1'> subtitle1 </Typography>
+      <Typography variant='subtitle2'> subtitle2 </Typography>
+
+      <Typography variant='body1'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nam aliquam unde consequatur dolores magnam ullam sit cum recusandae assumenda. </Typography>
+      <Typography variant='body2'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, velit!4 </Typography>
+
+
+    </div>
+  )
+}
+
+export default MuiTypographyComponent
